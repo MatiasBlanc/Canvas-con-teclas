53655c37d1fb28078c36d5daf35d56b1e394fb25
Matias Blanc <programador.matiasblanc@gmail.com>
2021-05-26 14:08:55 -0400

    add Readme

---
+++ readme.md
@@ -0,0 +1,11 @@
+### Teclas que debes ocupar
+> Las teclas que puedes ocupar para moverte y dibujar son:
+
+`w` o `k` para subir
+`s` o `j` para bajar
+`a` o `h` hacia la izquierda
+`d` o `l` hacia la derecha
+
+### Practica moverte como en neovim
+> ya que puedes ocupar `k j h l` puedes practicar y moverte con ellas 
+> Asi cuando ocupes nvim u otro similar lo haras mucho mejor
--- src/flechas.js
+++ src/flechas.js
@@ -20,19 +20,19 @@ function dibujarLinea(color, x_inicial, y_inicial, x_final, y_final, lienzo) {
 
 dibujarLinea("red", x-1, y-1, x+1, y+1, papel);
 document.addEventListener("keyup", function(e) {
-    if (e.key === 'w') {
+    if (e.key === 'w' || e.key === 'k') {
       dibujarLinea(colorLinea, x, y, x, y - mov, papel);
       y = y - mov;
     }
-    else if (e.key === 's') {
+    else if (e.key === 's' || e.key === 'j') {
       dibujarLinea(colorLinea, x, y, x, y + mov, papel);
       y = y + mov;
     }
-    else if (e.key === "a") {
+    else if (e.key === "a" || e.key === 'h') {
       dibujarLinea(colorLinea, x, y, x - mov, y, papel);
       x = x - mov;
     }
-    else if (e.key === "d") {
+    else if (e.key === "d" || e.key === 'l') {
       dibujarLinea(colorLinea, x, y, x + mov, y, papel);
       x = x + mov;
     }
